92f1a3c63b8eb95a8f6df40a0928eb4af8f19c91
RockyLama <lamadridpablo@gmail.com>
2022-05-20 18:48:41 -0300

    Update Account_typeController.java

--- apiCrypto/src/main/java/com/apiCrypto/apiCrypto/controller/Account_typeController.java
+++ apiCrypto/src/main/java/com/apiCrypto/apiCrypto/controller/Account_typeController.java
@@ -1,6 +1,7 @@
 package com.apiCrypto.apiCrypto.controller;
 
 
+import com.apiCrypto.apiCrypto.service.Account_typeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

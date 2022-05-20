17104bcc55845b900b0a71b727451d91f9f3dfca
RockyLama <lamadridpablo@gmail.com>
2022-05-20 10:23:38 -0300

    Update IPaymentRepository.java

--- apiCrypto/src/main/java/com/apiCrypto/apiCrypto/repository/IPaymentRepository.java
+++ apiCrypto/src/main/java/com/apiCrypto/apiCrypto/repository/IPaymentRepository.java
@@ -2,9 +2,11 @@ package com.apiCrypto.apiCrypto.repository;
 
 import com.apiCrypto.apiCrypto.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface IPaymentRepository extends JpaRepository<Payment, Long> {
     List<Payment> getbyIdUser(long id);
 

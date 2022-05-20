e7798f01c5d03350308a596f7e54a06f5ea4b0e8
RockyLama <lamadridpablo@gmail.com>
2022-05-20 18:48:32 -0300

    Update IAccount_typeRepository.java

--- apiCrypto/src/main/java/com/apiCrypto/apiCrypto/repository/IAccount_typeRepository.java
+++ apiCrypto/src/main/java/com/apiCrypto/apiCrypto/repository/IAccount_typeRepository.java
@@ -4,6 +4,11 @@ import com.apiCrypto.apiCrypto.model.Account_type;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IAccount_typeRepository extends JpaRepository<Account_type,Long>{
-    
+
+    List<Account_type> getbyIdAccount_type(long id_account_type);
+
+    void deleteByAccount_type(long id_account_type);
 }

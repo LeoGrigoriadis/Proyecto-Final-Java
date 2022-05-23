cad30c4b65ade1cd624a3c22478562874a34dd6e
AnaliaGuari <85644984+AnaliaGuari@users.noreply.github.com>
2022-05-23 18:57:10 -0300

    ahora si?

--- apiCrypto/src/main/java/com/apiCrypto/apiCrypto/repository/IAccount_typeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apiCrypto.apiCrypto.repository;
-
-import com.apiCrypto.apiCrypto.model.Account_type;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAccount_typeRepository extends JpaRepository<Account_type,Long>{
-    
-}

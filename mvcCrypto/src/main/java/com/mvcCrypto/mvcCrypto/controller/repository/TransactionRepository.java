12d60def21459231af11b60968f72aa5ae992cd0
Jeremias Roca <jereroca2@gmail.com>
2022-05-24 10:02:03 -0300

    add cobrar and depositar en MVC

--- mvcCrypto/src/main/java/com/mvcCrypto/mvcCrypto/controller/repository/TransactionRepository.java
+++ mvcCrypto/src/main/java/com/mvcCrypto/mvcCrypto/controller/repository/TransactionRepository.java
@@ -2,8 +2,10 @@ package com.mvcCrypto.mvcCrypto.controller.repository;
 
 import com.mvcCrypto.mvcCrypto.model.Transaction;
 import com.mvcCrypto.mvcCrypto.model.User;
+import com.mvcCrypto.mvcCrypto.model.User_Coin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -44,4 +46,28 @@ public class TransactionRepository {
         rt.put(url, es, Transaction.class);
     }
 
+
+    public void cobrar(User_Coin us,double balance, String id_coin, long id_user){
+        us.setBalance(balance);
+        us.setId_coin(id_coin);
+        us.setId_user(id_user);
+        String url = "http://localhost:8090/api/Transaction/CobrarMonto/"+balance+"/"+id_coin+"/"+id_user;
+        rt.put(url,us,User_Coin.class);
+    }
+
+    public void depositar(User_Coin us,double balance, String id_coin, long id_user){
+        us.setBalance(balance);
+        us.setId_coin(id_coin);
+        us.setId_user(id_user);
+        String url = "http://localhost:8090/api/Transaction/depositar/"+balance+"/"+id_coin+"/"+id_user;
+        rt.put(url,us,User_Coin.class);
+    }
+
+    public void cobrarTodo(User_Coin us, String id_coin, long id_user){
+
+        us.setId_coin(id_coin);
+        us.setId_user(id_user);
+        String url = "http://localhost:8090/api/Transaction/CobrarMonto/"+id_coin+"/"+id_user;
+        rt.put(url,us,User_Coin.class);
+    }
 }
--- mvcCrypto/src/main/java/com/mvcCrypto/mvcCrypto/controller/service/TransactionService.java
+++ mvcCrypto/src/main/java/com/mvcCrypto/mvcCrypto/controller/service/TransactionService.java
@@ -3,6 +3,7 @@ package com.mvcCrypto.mvcCrypto.controller.service;
 import com.mvcCrypto.mvcCrypto.controller.repository.TransactionRepository;
 import com.mvcCrypto.mvcCrypto.model.Transaction;
 
+import com.mvcCrypto.mvcCrypto.model.User_Coin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,4 +36,18 @@ public class TransactionService {
     public void update (Transaction transaction){
         tr.update(transaction);
     }
+
+    public void depositar(User_Coin us, double balance, String id_coin, long id_user){
+        tr.depositar(us,balance,id_coin,id_user);
+    }
+
+    public void cobrar(User_Coin us, double balance, String id_coin, long id_user){
+        tr.cobrar(us,balance,id_coin,id_user);
+    }
+
+    public void cobrarTodo(User_Coin us, String id_coin, long id_user){
+        tr.cobrarTodo(us,id_coin,id_user);
+    }
+    
+
 }

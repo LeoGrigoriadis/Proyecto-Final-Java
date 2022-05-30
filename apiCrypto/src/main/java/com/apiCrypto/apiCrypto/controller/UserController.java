704efc5f0cb812e06e6cdbb65ee5604c256f6565
AnaliaGuari <85644984+AnaliaGuari@users.noreply.github.com>
2022-05-30 18:17:06 -0300

    todo mal

--- apiCrypto/src/main/java/com/apiCrypto/apiCrypto/controller/UserController.java
+++ apiCrypto/src/main/java/com/apiCrypto/apiCrypto/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.apiCrypto.apiCrypto.controller;
 
 import java.util.List;
-
-
+import java.util.Optional;
 
 import com.apiCrypto.apiCrypto.model.User;
 import com.apiCrypto.apiCrypto.service.UserService;
@@ -22,7 +21,6 @@ public class UserController {
     public ResponseEntity<Object> getAll() {
         return ResponseEntity.status(200).body(us.getAll());
     }
-
 
      @PostMapping
     public ResponseEntity<Object> save(@RequestBody User u){
@@ -48,11 +46,8 @@ public class UserController {
     }
 
     @GetMapping("/getOne/{id}")
-    public ResponseEntity<Object> getOne(@PathVariable("id") Long id){
-            User user = us.getUser(id);
-
+    public ResponseEntity<Optional<User>> getOne(@PathVariable("id") Long id){
         return ResponseEntity.status(200).body(us.getUser(id));
-
     }
 
     @GetMapping("/find-by-gmail/{gmail}")
